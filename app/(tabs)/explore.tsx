e36669ca55ee96b262cd660bd5ee657787a8a61d
Gitartha kashyap <55341514+withewolf-dev@users.noreply.github.com>
2025-01-12 02:14:58 +0530

    Add Google Sign-In integration and update app configuration

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -6,7 +6,9 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
+GoogleSignin.configure();
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
